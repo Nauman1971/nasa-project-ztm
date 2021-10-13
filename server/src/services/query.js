d029c8b20dadf18a7a354832ef9bdd1dc3b72e23
Nauman <nauman81007@gmail.com>
2021-10-14 00:53:50 +0500

    Build pipeline workflow

---
+++ server/src/services/query.js
@@ -0,0 +1,19 @@
+// if page limit is set to zero mongoose will return all of the documents in the collection
+const DEFAULT_PAGE_LIMIT = 0;
+const DEFAULT_PAGE_NUMBER = 1;
+
+function getPagination(query) {
+    // make sure we are getting +ve and numbers only values.
+    const page = Math.abs(query.page) || DEFAULT_PAGE_NUMBER;
+    const limit = Math.abs(query.limit) || DEFAULT_PAGE_LIMIT;
+    const skip = (page - 1) * limit;
+
+    return {
+        skip,
+        limit
+    };
+};
+
+module.exports = {
+    getPagination,
+}
