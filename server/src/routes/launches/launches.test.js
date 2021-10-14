d329b39293d7007971fc6b3f5463f33139db00ce
Nauman <nauman81007@gmail.com>
2021-10-14 09:48:38 +0500

    Load planets data in tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -4,16 +4,19 @@ const {
     mongoConnect,
     mongoDisconnect
 } = require("../../services/mongo");
+const { loadPlanetData } = require('../../models/planets.model');
 
 describe('Launches API', () => {
 
     beforeAll(async () => {
         await mongoConnect();
+        await loadPlanetData();
     });
 
     afterAll(async () => {
         await mongoDisconnect();
-    })
+        await loadPlanetData();
+    });
 
     // describe are also called fixtures.
     describe('Test GET / launches', () => {
